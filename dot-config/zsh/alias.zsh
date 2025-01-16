9402fe7f344286171eafba69364b21d22277e551
Victor Garzon <victor.garzon@bain.com>
2025-01-16 10:07:23 -0800

    add vim and directory file count alias

--- dot-config/zsh/alias.zsh
+++ dot-config/zsh/alias.zsh
@@ -3,5 +3,7 @@ alias ls='lsd -F'
 alias ll='lsd -lF'
 alias dus='du -sh'
 alias dum='du -sm * | sort -nr'
+alias dfc='for dir in */; do echo "$(find "$dir" -type f | wc -l) "$dir""; done | sort -nr'
 alias dirs='dirs -v'
-
+alias vim='nvim'
+alias vi='nvim'
