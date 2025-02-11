013e1f8f0bf8f367ddfc0a7c67e6f2be2abbe51a
Victor Garzon <victor.garzon@bain.com>
2025-02-10 22:14:28 -0800

    Use fd instead of find

--- dot-config/zsh/local.zsh
+++ dot-config/zsh/local.zsh
@@ -24,3 +24,9 @@ export NVM_DIR="$HOME/.nvm"
 export VISUAL=nvim
 export EDITOR=$VISUAL
 export NODE_EXTRA_CA_CERTS=~/lib/cert/zscaler.pem
+
+# customize fzf shell integration
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude ".git" .'
+export FZF_CTRL_T_COMMAND='fd --hidden --follow --exclude ".git" .'
+export FZF_CTRL_T_OPTS="--preview 'bat --color=always {}'"
+export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude ".git" .'
