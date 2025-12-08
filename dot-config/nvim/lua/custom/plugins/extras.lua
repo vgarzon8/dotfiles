ab4cbd85eba16530cf22e4080a1c0685c4a09dc2
Victor Garzon <victor.garzon@bain.com>
2025-12-08 09:30:14 -0800

    add extra plugins file, e.g., gh copilot, oil.nvim

---
+++ dot-config/nvim/lua/custom/plugins/extras.lua
@@ -0,0 +1,72 @@
+return {
+  -- Navigate sbetween neovim and tmux panes / splits using consistent key mappings
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+      'TmuxNavigatorProcessList',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
+  },
+
+  -- GitHub Copilot plugin -- requires node.js
+  { 'github/copilot.vim' },
+
+  -- -- Oil.nvim is a file explorer that edits the filesystem like a normal Neovim buffer
+  -- {
+  --   'stevearc/oil.nvim',
+  --   ---@module 'oil'
+  --   ---@type oil.SetupOpts
+  --   opts = {},
+  --   -- Optional dependencies
+  --   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  --   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  --   config = function()
+  --     require('oil').setup {}
+  --     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+  --   end,
+  -- },
+
+  -- -- Snacks.nvim is a collection of small utilities for Neovim
+  -- {
+  --   'folke/snacks.nvim',
+  --   priority = 1000,
+  --   lazy = false,
+  --   ---@type snacks.Config
+  --   opts = {
+  --     -- your configuration comes here
+  --     -- or leave it empty to use the default settings
+  --     -- refer to the configuration section below
+  --     bigfile = { enabled = false },
+  --     dashboard = { enabled = true },
+  --     indent = { enabled = false },
+  --     input = { enabled = false },
+  --     notifier = { enabled = false },
+  --     quickfile = { enabled = false },
+  --     scroll = { enabled = false },
+  --     statuscolumn = { enabled = false },
+  --     words = { enabled = false },
+  --   },
+  -- },
+
+  -- -- Improve viewing of Markdown files
+  -- {
+  --   'MeanderingProgrammer/render-markdown.nvim',
+  --   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  --   ---@module 'render-markdown'
+  --   ---@type render.md.UserConfig
+  --   opts = {},
+  -- },
+}
