339a0c71a28fd07b7aa3a149930dc35a874b665e
Victor Garzon <victor.garzon@bain.com>
2026-01-01 21:25:27 -0800

    nvim: move autocommands to separate file

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,23 +1,6 @@
 require 'custom.core.options' -- Set basic options
 require 'custom.core.keymaps' -- Set basic keymaps
-
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.hl.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
-})
-
--- TODO: Find permanent fix
--- Disable Python 3 provider for faster startup if not needed
-vim.g.loaded_python3_provider = 0
+require 'custom.core.autocmds' -- Set basic autocommands
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
---
+++ dot-config/nvim/lua/custom/core/autocmds.lua
@@ -0,0 +1,17 @@
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- TODO: Find permanent fix
+-- Disable Python 3 provider for faster startup if not needed
+vim.g.loaded_python3_provider = 0
