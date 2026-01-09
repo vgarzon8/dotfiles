15672a478d759d92bd90f486abfa141716ed8055
Victor Garzon <victor.garzon@bain.com>
2026-01-08 16:01:55 -0800

    nvim: set options with vim.opt instead of vim.o

--- dot-config/nvim/lua/custom/core/options.lua
+++ dot-config/nvim/lua/custom/core/options.lua
@@ -10,52 +10,51 @@ vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
+--
+-- See `:help options` , `:help option-list`, `:help vim.opt` and `:help vim.o`
+local opt = vim.opt
 -- Make line numbers default
-vim.o.number = true
+opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
-vim.o.relativenumber = true
+opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
+opt.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
-vim.o.showmode = false
+opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
+  opt.clipboard = 'unnamedplus'
 end)
 
 -- Enable break indent
-vim.o.breakindent = true
+opt.breakindent = true
 
 -- Save undo history
-vim.o.undofile = true
+opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.o.signcolumn = 'yes'
+opt.signcolumn = 'yes'
 
 -- Decrease update time
-vim.o.updatetime = 250
+opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
-vim.o.timeoutlen = 300
+opt.timeoutlen = 300
 
 -- Configure how new splits should be opened
-vim.o.splitright = true
-vim.o.splitbelow = true
+opt.splitright = true
+opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
@@ -65,34 +64,34 @@ vim.o.splitbelow = true
 --  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
 --   See `:help lua-options`
 --   and `:help lua-options-guide`
-vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
+opt.inccommand = 'split'
 
 -- Show which line your cursor is on
-vim.o.cursorline = true
+opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
+opt.scrolloff = 10
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
-vim.o.confirm = true
+opt.confirm = true
 
 -- Set default border for floating windoes
-vim.o.winborder = 'rounded'
+opt.winborder = 'rounded'
 
 -- set `:help termguicolors`
-vim.o.termguicolors = true
+opt.termguicolors = true
 
--- set external grep program
+-- set external grep program -- see `RgFiiles` in lua/custom/core/user_commands.lua
 if vim.fn.executable 'rg' == 1 then
-  vim.o.grepprg = 'rg --vimgrep --smart-case'
-  vim.o.grepformat = '%f:%l:%c:%m'
+  opt.grepprg = 'rg --vimgrep --smart-case'
+  opt.grepformat = '%f:%l:%c:%m'
 else
-  vim.o.grepprg = 'grep -RIn'
-  vim.o.grepformat = '%f:%l:%c:%m'
+  opt.grepprg = 'grep -RIn'
+  opt.grepformat = '%f:%l:%c:%m'
 end
